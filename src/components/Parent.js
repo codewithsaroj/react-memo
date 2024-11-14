1f0616f337d0006a80cc2d41486df0cde3e60936
Saroj <thesarojkumar99@gmail.com>
2024-11-15 00:36:04 +0530

    update in parent component

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -5,7 +5,14 @@ const Parent = () => {
   console.log("Parent renders");
   const [todos, setTodos] = useState(["task 1", "task 2"]);
   const [count, setCount] = useState(0);
+
+  const expensiveCalFn = (count) => {
+    for (let i = 0; i < 1000000000; i++) {}
+    return count + 1;
+  };
+
   const expensiveCalculation = useMemo(() => expensiveCalFn(count), [count]);
+
   const increment = () => {
     setCount((count) => count + 1);
   };
@@ -15,20 +22,13 @@ const Parent = () => {
   }, [todos]);
   return (
     <div>
-      <p>Expensive {expensiveCalculation}</p>
-      <p>Count - {count}</p>
+      <p>Expensive Calculation : {expensiveCalculation}</p>
+      <p>Count : {count}</p>
       <button onClick={increment}>Increment Count</button>
-      <br />
-
       <hr />
       <Child todos={todos} addTodo={addTodo} />
     </div>
   );
-};
-
-const expensiveCalFn = (count) => {
-  for (let i = 0; i < 1000000000; i++) {}
-  return count + 1;
 };
 
 export default Parent;
